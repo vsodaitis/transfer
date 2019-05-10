9149ed679eb2df61697166e9b71e1f4045b7f6f5
Vytenis Sodaitis <v.sodaitis@ba.lt>
2019-05-10 16:33:29 +0300

    prepare for tests

--- src/main/java/com/vytenis/transfer/resource/AccountsResource.java
+++ src/main/java/com/vytenis/transfer/resource/AccountsResource.java
@@ -4,11 +4,15 @@ import com.vytenis.transfer.dto.Account;
 import com.vytenis.transfer.dto.User;
 import com.vytenis.transfer.service.AccountService;
 import com.vytenis.transfer.service.UserService;
+import io.quarkus.runtime.StartupEvent;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
 import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 @Path("/accounts")
@@ -26,6 +30,7 @@ public class AccountsResource {
     @GET
     @Path("/{userId}")
     public List<Account> getAccounts(@PathParam("userId") Long userId) {
+        System.out.println(new SimpleDateFormat("HH:mm:ss.SSS").format(new Date()));
         User user = userService.getUser(userId);
         if (user == null) {
             throw new IllegalArgumentException("User not found");
@@ -36,5 +41,9 @@ public class AccountsResource {
     @POST
     public long addAccount(Account account) {
         return accountService.addUserAccount(account);
+    }
+
+    public void onStart(@Observes StartupEvent startupEvent) {
+        System.out.println(new SimpleDateFormat("HH:mm:ss.SSS").format(new Date()));
     }
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,13 +1,13 @@
-INSERT INTO userentity (id, fullname) VALUES (NEXT VALUE FOR hibernate_sequence, 'Viktor Tsoy');
+INSERT INTO userentity (id, fullname) VALUES (nextval('hibernate_sequence'), 'Viktor Tsoy');
 
-INSERT INTO balanceentity (id, total, reserved) VALUES (NEXT VALUE FOR hibernate_sequence, 100, 90);
-INSERT INTO balanceentity (id, total, reserved) VALUES (NEXT VALUE FOR hibernate_sequence, 600, 11);
+INSERT INTO balanceentity (id, total, reserved) VALUES (nextval('hibernate_sequence'), 100, 90);
+INSERT INTO balanceentity (id, total, reserved) VALUES (nextval('hibernate_sequence'), 600, 11);
 
-INSERT INTO accountentity (id, user_id, status, balance_id, iban, currency) VALUES (NEXT VALUE FOR hibernate_sequence, 1, 'ACTIVE', 2, 'LT4645464564', 'EUR');
-INSERT INTO accountentity (id, user_id, status, balance_id, iban, currency) VALUES (NEXT VALUE FOR hibernate_sequence, 1, 'ACTIVE', 3, 'RB3151564887', 'USD');
+INSERT INTO accountentity (id, user_id, status, balance_id, iban, currency) VALUES (nextval('hibernate_sequence'), 1, 'ACTIVE', 2, 'LT4645464564', 'EUR');
+INSERT INTO accountentity (id, user_id, status, balance_id, iban, currency) VALUES (nextval('hibernate_sequence'), 1, 'ACTIVE', 3, 'RB3151564887', 'USD');
 
-INSERT INTO userentity (id, fullname) Values (NEXT VALUE FOR hibernate_sequence, 'John Smith');
+INSERT INTO userentity (id, fullname) Values (nextval('hibernate_sequence'), 'John Smith');
 
-INSERT INTO balanceentity (id, total, reserved) VALUES (NEXT VALUE FOR hibernate_sequence, 500, 0);
+INSERT INTO balanceentity (id, total, reserved) VALUES (nextval('hibernate_sequence'), 500, 0);
 
-INSERT INTO accountentity (id, user_id, status, balance_id, iban, currency) VALUES (NEXT VALUE FOR hibernate_sequence, 6, 'ACTIVE', 7, 'LT3212131544', 'EUR');
+INSERT INTO accountentity (id, user_id, status, balance_id, iban, currency) VALUES (nextval('hibernate_sequence'), 6, 'ACTIVE', 7, 'LT3212131544', 'EUR');
